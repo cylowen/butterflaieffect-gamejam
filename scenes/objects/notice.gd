30756cef7935782c3f5ea0b5ffb5ff4fc8327b22
cylowen <chris.vogel@mailbox.org>
2025-02-02 00:12:09 +0100

    notice debug
    working now

--- scenes/objects/notice.gd
+++ scenes/objects/notice.gd
@@ -5,23 +5,27 @@ extends XRToolsPickable
 @onready var good_sprite_3d: Sprite3D = $GoodSprite3D
 
 
+
 func _ready() -> void:
 	SignalManager.connect("scene_changed", _on_scene_changed)
 
 
 func _on_scene_changed() -> void:
+	
 	if GameManager.NOTICE_STATE == "start":
 		start_sprite_3d.visible = true
 		worst_sprite_3d.visible = false
-		#good_sprite_3d.visibile = false
+		good_sprite_3d.visible = false
 		print("change sprite")
-	if GameManager.NOTICE_STATE == "good":
+		
+	elif GameManager.NOTICE_STATE == "good":
 		start_sprite_3d.visible = false
 		worst_sprite_3d.visible = false
-		#good_sprite_3d.visibile = true
+		good_sprite_3d.visible = true
 		print("change sprite")
-	else:
+		
+	elif GameManager.NOTICE_STATE == "worst":
 		start_sprite_3d.visible = false
 		worst_sprite_3d.visible = true
-		#good_sprite_3d.visibile = false
+		good_sprite_3d.visible = false
 		print("change sprite")
--- scenes/objects/notice_text.gd
+++ scenes/objects/notice_text.gd
@@ -6,17 +6,21 @@ func _ready() -> void:
 
 
 func _on_scene_changed() -> void:
+	
 	if GameManager.NOTICE_STATE == "start":
 		$ControlStart.visible = true
 		$ControlGood.visible = false
 		$ControlWorse.visible = false
+	
 	if GameManager.NOTICE_STATE == "good":
 		$ControlStart.visible = false
 		$ControlGood.visible = true
 		$ControlWorse.visible = false
+	
 	if GameManager.NOTICE_STATE == "worst":
 		$ControlStart.visible = false
 		$ControlGood.visible = false
 		$ControlWorse.visible = true
+	
 	else:
 		print("could not change state of notice")
