dcef5fd50d01632c7b38532675bee5f450f0532d
cylowen <chris.vogel@mailbox.org>
2025-02-02 10:56:29 +0100

    added ukulele and assets
    added ukulele and assets

--- background.gd
+++ background.gd
@@ -2,6 +2,7 @@ extends Sprite3D
 
 const FENSTER_VIEW_BAD_CHOICE = preload("res://art/pictures/objects/window/FEnster_VIEW-BAD-Choice-NEW.jpeg")
 const FENSTER_VIEW_START_CHOICE = preload("res://art/pictures/objects/window/Fenster_VIEW-Start-choice-NEW.png")
+const FENSTER_VIEW_GOOD_CHOICE = preload("res://art/pictures/objects/window/Fenster_View-GOOD-choice.png")
 
 func _ready() -> void:
 	SignalManager.connect("scene_changed", _on_scene_changed)
@@ -12,7 +13,7 @@ func _on_scene_changed() -> void:
 		texture = FENSTER_VIEW_START_CHOICE
 		print("change window")
 	if GameManager.WINDOW_STATE == "good":
-		texture = FENSTER_VIEW_START_CHOICE
+		texture = FENSTER_VIEW_GOOD_CHOICE
 		print("change window")
 	if GameManager.WINDOW_STATE == "worst":
 		texture = FENSTER_VIEW_BAD_CHOICE
